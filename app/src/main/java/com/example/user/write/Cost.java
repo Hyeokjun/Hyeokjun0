8af106da8f7fd1aea6bf3426c6bbfa4dc3099a51
Hyeokjun <ann10624@naver.com>
2016-07-07 17:06:27 +0900

    로그인화면 연동안됨

---
+++ app/src/main/java/com/example/user/write/Cost.java
@@ -0,0 +1,13 @@
+package com.example.user.write;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Cost extends Activity{
+@Override
+    protected void onCreate(Bundle savedInstanceState)
+{
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_sub0);
+}
+}
